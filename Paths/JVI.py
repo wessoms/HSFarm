fa707eee4dc56e68bc9bce1029a155a0d2e9c193
wessoms <sessoms.will@gmail.com>
2023-12-23 16:26:45 -0500

    Fixed Comments

--- Paths/JVI.py
+++ Paths/JVI.py
@@ -1,10 +1,10 @@
 from .JVISupport import *
 
 def fullClear():
-    #AreaClear.clearOSP()
-    #AreaClear.clearBP()
-    #AreaClear.clearCFE()
-    #AreaClear.clearEH()
-    #AreaClear.clearGM()
-    #AreaClear.clearRT()
+    AreaClear.clearOSP()
+    AreaClear.clearBP()
+    AreaClear.clearCFE()
+    AreaClear.clearEH()
+    AreaClear.clearGM()
+    AreaClear.clearRT()
     AreaClear.clearRS()
